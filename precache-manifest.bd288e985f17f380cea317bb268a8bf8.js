ab8d2f4d4607eb6d543baabb3b0de6d4545e14de
Metal Whale <metalwhale.com@gmail.com>
2019-06-12 07:13:03 +0000

    Updates

---
+++ precache-manifest.bd288e985f17f380cea317bb268a8bf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceac4bb60e9ee7db3036fe999e527b5a",
+    "url": "/etl8b/index.html"
+  },
+  {
+    "revision": "5e6550305e7c5f5afb5e",
+    "url": "/etl8b/static/css/main.11abd45c.chunk.css"
+  },
+  {
+    "revision": "30b5a4e3ddc1146cf0ae",
+    "url": "/etl8b/static/js/2.ccf73963.chunk.js"
+  },
+  {
+    "revision": "5e6550305e7c5f5afb5e",
+    "url": "/etl8b/static/js/main.a69c07f9.chunk.js"
+  },
+  {
+    "revision": "0dac065fa800f1345e26",
+    "url": "/etl8b/static/js/runtime~main.6a052c90.js"
+  }
+]);
